5f06c90ef93310c18ce42370f0a9adc066f62967
Dominic van Berkel <dominic@baudvine.net>
2021-12-04 14:00:02 +0100

    non-ringy at()

--- include/baudvine/ringbuf/ringbuf.h
+++ include/baudvine/ringbuf/ringbuf.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <vector>
+
 namespace baudvine {
 
 template <typename Elem, size_t Size> class ringbuf {
 public:
+  ringbuf() : Data(Size) {}
+
   constexpr size_t size() const noexcept { return Size; }
+  const Elem &at(size_t index) const { return Data.at(index); }
+  Elem &at(size_t index) { return Data.at(index); }
+
+private:
+  std::vector<Elem> Data;
 };
 
 } // namespace baudvine
--- test/test_ringbuf.cpp
+++ test/test_ringbuf.cpp
@@ -2,7 +2,7 @@
 
 #include <baudvine/ringbuf/ringbuf.h>
 
-TEST(Create, Size) {
+TEST(Size, Size) {
   EXPECT_EQ((baudvine::ringbuf<int, 0>){}.size(), 0);
   EXPECT_EQ((baudvine::ringbuf<int, 1>){}.size(), 1);
   EXPECT_EQ((baudvine::ringbuf<int, 128>){}.size(), 128);
@@ -11,4 +11,30 @@ TEST(Create, Size) {
             10 * 1024 * 1024);
 
   EXPECT_EQ((baudvine::ringbuf<char, 128>){}.size(), 128);
+}
+
+TEST(At, Empty) {
+  baudvine::ringbuf<int, 4> underTest;
+  for (size_t i = 0; i < 4; i++) {
+    EXPECT_NO_THROW(underTest.at(i));
+  }
+}
+
+TEST(At, OutOfBounds)
+{
+  baudvine::ringbuf<int, 4> underTest;
+  EXPECT_THROW(underTest.at(4), std::out_of_range);
+}
+
+TEST(AtConst, Empty) {
+  const baudvine::ringbuf<int, 4> underTest;
+  for (size_t i = 0; i < 4; i++) {
+    EXPECT_NO_THROW(underTest.at(i));
+  }
+}
+
+TEST(AtConst, OutOfBounds)
+{
+  const baudvine::ringbuf<int, 4> underTest;
+  EXPECT_THROW(underTest.at(4), std::out_of_range);
 }
