7c7c0d7f4e4be704a26efdc856a72e2f421b7279
Dominic van Berkel <dominic@baudvine.net>
2021-12-04 13:37:28 +0100

    Implement ringbuf::size

--- include/baudvine/ringbuf/ringbuf.h
+++ include/baudvine/ringbuf/ringbuf.h
@@ -2,6 +2,9 @@
 
 namespace baudvine {
 
-template <typename Elem, size_t Size> class ringbuf {};
+template <typename Elem, size_t Size> class ringbuf {
+public:
+  constexpr size_t size() const noexcept { return Size; }
+};
 
 } // namespace baudvine
--- test/test_ringbuf.cpp
+++ test/test_ringbuf.cpp
@@ -2,7 +2,13 @@
 
 #include <baudvine/ringbuf/ringbuf.h>
 
-TEST(Create, Create)
-{
-  baudvine::ringbuf<int, 128>{};
+TEST(Create, Size) {
+  EXPECT_EQ((baudvine::ringbuf<int, 0>){}.size(), 0);
+  EXPECT_EQ((baudvine::ringbuf<int, 1>){}.size(), 1);
+  EXPECT_EQ((baudvine::ringbuf<int, 128>){}.size(), 128);
+  EXPECT_EQ((baudvine::ringbuf<int, 500>){}.size(), 500);
+  EXPECT_EQ((baudvine::ringbuf<int, 10 * 1024 * 1024>){}.size(),
+            10 * 1024 * 1024);
+
+  EXPECT_EQ((baudvine::ringbuf<char, 128>){}.size(), 128);
 }
