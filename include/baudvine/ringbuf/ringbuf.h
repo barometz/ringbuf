63253610678011e43ea8e521dbfcc0e4cc5c249b
Dominic van Berkel <dominic@baudvine.net>
2021-12-04 14:48:18 +0100

    the buffer rings!

--- include/baudvine/ringbuf/ringbuf.h
+++ include/baudvine/ringbuf/ringbuf.h
@@ -10,15 +10,37 @@ template <typename Elem, size_t Capacity> class ringbuf {
 public:
   ringbuf() : Data(Capacity) {}
 
-  // TODO hang on, size and capacity aren't the same
   constexpr size_t capacity() const noexcept { return Capacity; }
-  const Elem &at(size_t index) const { return Data.at(index); }
-  Elem &at(size_t index) { return Data.at(index); }
+  size_t size() const noexcept { return Size; }
+  const Elem &at(size_t index) const {
+    if (index >= Size) {
+      throw std::out_of_range("ringbuf::at: index >= Size");
+    }
+    return Data.at((Base + index) % Capacity);
+  }
+  Elem &at(size_t index) {
+    if (index >= Size) {
+      throw std::out_of_range("ringbuf::at: index >= Size");
+    }
+    return Data.at((Base + index) % Capacity);
+  }
 
-  // void push(const T& value) { }
+  void push(const Elem &value) {
+    Data.at(Next) = value;
+
+    if (Size == Capacity)
+      Base = (Base + 1) % Capacity;
+
+    if (Size != Capacity)
+      Size++;
+
+    Next = (Next + 1) % Capacity;
+  }
 
 private:
   std::vector<Elem> Data;
+  size_t Base{0U};
+  size_t Next{0U};
   size_t Size{0U};
 };
 
--- test/test_ringbuf.cpp
+++ test/test_ringbuf.cpp
@@ -15,25 +15,44 @@ TEST(Capacity, Capacity) {
 
 TEST(At, Empty) {
   baudvine::ringbuf<int, 4> underTest;
-  for (size_t i = 0; i < 4; i++) {
-    EXPECT_NO_THROW(underTest.at(i));
-  }
+
+  EXPECT_THROW(underTest.at(0), std::out_of_range);
+  EXPECT_THROW(underTest.at(1), std::out_of_range);
   EXPECT_THROW(underTest.at(4), std::out_of_range);
 }
 
 TEST(AtConst, Empty) {
   const baudvine::ringbuf<int, 4> underTest;
-  for (size_t i = 0; i < 4; i++) {
-    EXPECT_NO_THROW(underTest.at(i));
-  }
+
+  EXPECT_THROW(underTest.at(0), std::out_of_range);
+  EXPECT_THROW(underTest.at(1), std::out_of_range);
   EXPECT_THROW(underTest.at(4), std::out_of_range);
 }
 
-// TEST(Push, Push){
-//   const baudvine::ringbuf<int, 4> underTest;
-//   underTest.push(56);
-//   EXPECT_EQ(underTest.at(0), 56);
-//   underTest.push(1100);
-//   EXPECT_EQ(underTest.at(0), 1100);
-//   EXPECT_EQ(underTest.at(1), 56);
-// }
+TEST(Push, Push){
+  baudvine::ringbuf<int, 4> underTest;
+  underTest.push(56);
+  EXPECT_EQ(underTest.at(0), 56);
+  underTest.push(1100);
+  EXPECT_EQ(underTest.at(1), 1100);
+  EXPECT_EQ(underTest.at(0), 56);
+  EXPECT_EQ(underTest.size(), 2);
+}
+
+TEST(Push, PushOver){
+  baudvine::ringbuf<int, 2> underTest;
+  underTest.push(56);
+  underTest.push(1100);
+  underTest.push(6500);
+
+  EXPECT_EQ(underTest.size(), 2);
+  EXPECT_EQ(underTest.at(0), 1100);
+  EXPECT_EQ(underTest.at(1), 6500);
+
+  underTest.push(10);
+  underTest.push(12);
+  underTest.push(18);
+  EXPECT_EQ(underTest.size(), 2);
+  EXPECT_EQ(underTest.at(0), 12);
+  EXPECT_EQ(underTest.at(1), 18);
+}
