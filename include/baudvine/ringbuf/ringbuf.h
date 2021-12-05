e7e8b79a387bdc5efb09aeadda8628b4ba05dedb
Dominic van Berkel <dominic@baudvine.net>
2021-12-05 11:01:09 +0100

    Remove RingBufBase

--- include/baudvine/ringbuf/ringbuf.h
+++ include/baudvine/ringbuf/ringbuf.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cassert>
+#include <cstddef>
 #include <stdexcept>
 #include <tuple>
 
@@ -11,21 +12,136 @@ namespace detail {
 // TODO: move params, emplace smarts
 // TODO: reverse iterator
 // TODO: decide between front/at/??
+// TODO: std::copy optimization?
+
+template <std::size_t Capacity>
+constexpr std::size_t RingWrap(std::size_t position) {
+  return position % Capacity;
+}
+template <>
+constexpr std::size_t RingWrap<0>(std::size_t /*position*/) {
+  return 0;
+}
 
 template <typename Elem, std::size_t Capacity>
-class RingBufBase {
+class ConstIterator {
+ public:
+  using difference_type = std::ptrdiff_t;
+  using value_type = Elem;
+  using pointer = const Elem*;
+  using reference = const Elem&;
+  using iterator_category = std::forward_iterator_tag;
+
+  constexpr ConstIterator() noexcept {}
+  ConstIterator(pointer data, std::size_t base, std::size_t position) noexcept
+      : data_(data), base_(base), position_(position) {}
+
+  reference operator*() const {
+    return data_[RingWrap<Capacity>(base_ + position_)];
+  }
+
+  ConstIterator operator++(int) noexcept {
+    ConstIterator copy(*this);
+    operator++();
+    return copy;
+  }
+
+  ConstIterator& operator++() noexcept {
+    ++position_;
+    return *this;
+  }
+
+  friend bool operator<(const ConstIterator& lhs,
+                        const ConstIterator& rhs) noexcept {
+    return std::tie(lhs.data_, lhs.base_, lhs.position_) <
+           std::tie(rhs.data_, rhs.base_, rhs.position_);
+  }
+
+  friend bool operator==(const ConstIterator& lhs,
+                         const ConstIterator& rhs) noexcept {
+    return std::tie(lhs.data_, lhs.base_, lhs.position_) ==
+           std::tie(rhs.data_, rhs.base_, rhs.position_);
+  }
+
+  friend bool operator!=(const ConstIterator& lhs,
+                         const ConstIterator& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+ private:
+  pointer data_{};
+  std::size_t base_{};
+  std::size_t position_{};
+};
+
+template <typename Elem, size_t Capacity>
+class Iterator {
+ public:
+  using difference_type = std::ptrdiff_t;
+  using value_type = Elem;
+  using pointer = Elem*;
+  using reference = Elem&;
+  using iterator_category = std::forward_iterator_tag;
+
+  Iterator() {}
+  Iterator(pointer data, std::size_t base, size_t position)
+      : data_(data), base_(base), position_(position) {}
+
+  operator ConstIterator<value_type, Capacity>() const {
+    return ConstIterator<value_type, Capacity>(data_, base_, position_);
+  }
+
+  reference operator*() const {
+    return data_[RingWrap<Capacity>(base_ + position_)];
+  }
+
+  Iterator operator++(int) noexcept {
+    Iterator copy(*this);
+    operator++();
+    return copy;
+  }
+
+  Iterator& operator++() noexcept {
+    ++position_;
+    return *this;
+  }
+
+  friend bool operator<(const Iterator& lhs, const Iterator& rhs) noexcept {
+    return std::tie(lhs.data_, lhs.base_, lhs.position_) <
+           std::tie(rhs.data_, rhs.base_, rhs.position_);
+  }
+
+  friend bool operator==(const Iterator& lhs, const Iterator& rhs) noexcept {
+    return std::tie(lhs.data_, lhs.base_, lhs.position_) ==
+           std::tie(rhs.data_, rhs.base_, rhs.position_);
+  }
+
+  friend bool operator!=(const Iterator& lhs, const Iterator& rhs) noexcept {
+    return !(lhs == rhs);
+  }
+
+ private:
+  pointer data_{};
+  std::size_t base_{};
+  std::size_t position_{};
+};
+
+}  // namespace detail
+
+template <typename Elem, size_t Capacity>
+class RingBuf {
  public:
   using value_type = Elem;
   using reference = Elem&;
   using const_reference = const Elem&;
+  using iterator = detail::Iterator<Elem, Capacity>;
+  using const_iterator = detail::ConstIterator<Elem, Capacity>;
+  using difference_type = typename iterator::difference_type;
   using size_type = std::size_t;
 
+  constexpr size_type max_size() const noexcept { return Capacity; }
   size_type size() const noexcept { return size_; }
-
-  const_reference operator[](size_type index) const {
-    return data_[Wrap(base_ + index)];
-  }
-  reference operator[](size_type index) { return data_[Wrap(base_ + index)]; }
+  bool empty() const noexcept { return this->size() == 0; }
 
   void push_back(const_reference value) {
     if (Capacity == 0) {
@@ -37,7 +153,7 @@ class RingBufBase {
 
     // The base only moves when we're full.
     if (size_ == Capacity) {
-      base_ = Wrap(base_ + 1);
+      base_ = detail::RingWrap<Capacity>(base_ + 1);
     }
 
     // Size will never exceed the capacity.
@@ -51,149 +167,29 @@ class RingBufBase {
       return;
     }
 
-    base_ = Wrap(base_ + 1);
+    base_ = detail::RingWrap<Capacity>(base_ + 1);
     size_--;
   }
 
- private:
-  std::array<value_type, Capacity> data_;
-  size_type base_{0U};
-  size_type size_{0U};
-
-  size_type GetNext() { return Wrap(base_ + size_); }
-
-  template <size_type C = Capacity>
-  static constexpr size_type Wrap(size_type position) {
-    return position % C;
+  iterator begin() noexcept {
+    return iterator(&this->data_[0], this->base_, 0);
   }
-  template <>
-  static constexpr size_type Wrap<0>(size_type /*position*/) {
-    return 0;
+  iterator end() noexcept {
+    return iterator(&this->data_[0], this->base_, this->size());
   }
-};
-
-template <typename Elem, std::size_t Capacity>
-class ConstIterator {
- public:
-  using difference_type = std::ptrdiff_t;
-  using value_type = Elem;
-  using pointer = const Elem*;
-  using reference = const Elem&;
-  using iterator_category = std::forward_iterator_tag;
-
-  constexpr ConstIterator() noexcept {}
-  ConstIterator(const RingBufBase<Elem, Capacity>& ring_buf,
-                std::size_t position) noexcept
-      : ring_buf_(&ring_buf), position_(position) {}
-
-  reference operator*() const { return (*ring_buf_)[position_]; }
-
-  ConstIterator operator++(int) noexcept {
-    ConstIterator copy(*this);
-    operator++();
-    return copy;
+  const_iterator cbegin() const noexcept {
+    return const_iterator(&this->data_[0], this->base_, 0);
   }
-
-  ConstIterator& operator++() noexcept {
-    position_ = std::min(position_ + 1, ring_buf_->size());
-    return *this;
-  }
-
-  friend bool operator<(const ConstIterator& lhs,
-                        const ConstIterator& rhs) noexcept {
-    return std::tie(lhs.ring_buf_, lhs.position_) <
-           std::tie(rhs.ring_buf_, rhs.position_);
-  }
-
-  friend bool operator==(const ConstIterator& lhs,
-                         const ConstIterator& rhs) noexcept {
-    return std::tie(lhs.ring_buf_, lhs.position_) ==
-           std::tie(rhs.ring_buf_, rhs.position_);
-  }
-
-  friend bool operator!=(const ConstIterator& lhs,
-                         const ConstIterator& rhs) noexcept {
-    return !(lhs == rhs);
-  }
-
- private:
-  const RingBufBase<Elem, Capacity>* ring_buf_{};
-  size_t position_{};
-};
-
-template <typename Elem, size_t Capacity>
-class Iterator {
- public:
-  using difference_type = std::ptrdiff_t;
-  using value_type = Elem;
-  using pointer = Elem*;
-  using reference = Elem&;
-  using iterator_category = std::forward_iterator_tag;
-
-  Iterator() {}
-  Iterator(RingBufBase<value_type, Capacity>& ring_buf, size_t position)
-      : ring_buf_(&ring_buf), position_(position) {}
-
-  operator ConstIterator<value_type, Capacity>() const {
-    return ConstIterator<value_type, Capacity>(*ring_buf_, position_);
-  }
-
-  reference operator*() const { return (*ring_buf_)[position_]; }
-
-  Iterator operator++(int) noexcept {
-    Iterator copy(*this);
-    operator++();
-    return copy;
-  }
-
-  Iterator& operator++() noexcept {
-    position_ = std::min(position_ + 1, ring_buf_->size());
-    return *this;
-  }
-
-  friend bool operator<(const Iterator& lhs, const Iterator& rhs) noexcept {
-    return std::tie(lhs.ring_buf_, lhs.position_) <
-           std::tie(rhs.ring_buf_, rhs.position_);
-  }
-
-  friend bool operator==(const Iterator& lhs, const Iterator& rhs) noexcept {
-    return std::tie(lhs.ring_buf_, lhs.position_) ==
-           std::tie(rhs.ring_buf_, rhs.position_);
-  }
-
-  friend bool operator!=(const Iterator& lhs, const Iterator& rhs) noexcept {
-    return !(lhs == rhs);
-  }
-
- private:
-  RingBufBase<value_type, Capacity>* ring_buf_{};
-  size_t position_{};
-};
-
-}  // namespace detail
-
-template <typename Elem, size_t Capacity>
-class RingBuf : public detail::RingBufBase<Elem, Capacity> {
- public:
-  using reference = Elem&;
-  using const_reference = const Elem&;
-  using size_type = typename RingBuf::size_type;
-  using iterator = detail::Iterator<Elem, Capacity>;
-  using const_iterator = detail::ConstIterator<Elem, Capacity>;
-  using difference_type = typename iterator::difference_type;
-
-  constexpr size_type max_size() const noexcept { return Capacity; }
-
-  bool empty() const noexcept { return this->size() == 0; }
-
-  iterator begin() noexcept { return iterator(*this, 0); }
-  iterator end() noexcept { return iterator(*this, this->size()); }
-  const_iterator cbegin() const noexcept { return const_iterator(*this, 0); }
   const_iterator cend() const noexcept {
-    return const_iterator(*this, this->size());
+    return const_iterator(&this->data_[0], this->base_, this->size());
   }
 
-  void swap(RingBuf& other) { std::swap(*this, other); }
+  const_reference operator[](size_type index) const {
+    return data_[detail::RingWrap<Capacity>(base_ + index)];
+  }
+  reference operator[](size_type index) {
+    return data_[detail::RingWrap<Capacity>(base_ + index)];
+  }
 
   const_reference at(size_type index) const {
     if (index >= this->size()) {
@@ -208,6 +204,8 @@ class RingBuf : public detail::RingBufBase<Elem, Capacity> {
     return (*this)[index];
   }
 
+  void swap(RingBuf& other) { std::swap(*this, other); }
+
   friend bool operator==(const RingBuf& lhs, const RingBuf& rhs) {
     if (lhs.size() != rhs.size())
       return false;
@@ -220,6 +218,13 @@ class RingBuf : public detail::RingBufBase<Elem, Capacity> {
   friend bool operator!=(const RingBuf& lhs, const RingBuf& rhs) {
     return !(lhs == rhs);
   }
+
+ private:
+  value_type data_[Capacity]{};
+  size_type base_{0U};
+  size_type size_{0U};
+
+  size_type GetNext() { return detail::RingWrap<Capacity>(base_ + size_); }
 };
 
 }  // namespace baudvine
