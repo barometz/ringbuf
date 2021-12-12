18453f0ec3db8488d12ef5fe69eeb32f3e642496
Dominic van Berkel <dominic@baudvine.net>
2021-12-12 15:43:29 +0100

    Kill some g++ warnings

--- test/ringbuf_adapter.h
+++ test/ringbuf_adapter.h
@@ -11,12 +11,7 @@ enum class Variant {
 };
 
 inline std::ostream& operator<<(std::ostream& os, Variant variant) {
-  switch (variant) {
-    case Variant::Standard:
-      return os << "Standard";
-    case Variant::Deque:
-      return os << "Deque";
-  }
+  return os << (variant == Variant::Standard ? "Standard" : "Deque");
 }
 
 // Adapter for the two ringbuf implementations, so one set of tests can test
@@ -27,68 +22,47 @@ class RingBufAdapter {
  public:
   RingBufAdapter(Variant variant) : variant_(variant) {}
 
-#define DISPATCH(call)          \
-  do {                          \
-    switch (variant_) {         \
-      case (Variant::Standard): \
-        return static_.call;    \
-      case (Variant::Deque):    \
-        return dynamic_.call;   \
-    }                           \
-  } while (false)
+#define DISPATCH(call) \
+  variant_ == Variant::Standard ? static_.call : dynamic_.call
 
-  Elem& front() { DISPATCH(front()); }
-  Elem& back() { DISPATCH(back()); }
-  Elem& operator[](size_t index) { DISPATCH(operator[](index)); }
-  const Elem& operator[](size_t index) const { DISPATCH(operator[](index)); }
-  Elem& at(size_t index) { DISPATCH(at(index)); }
-  const Elem& at(size_t index) const { DISPATCH(at(index)); }
-  void clear() { DISPATCH(clear()); }
-  void push_back(const Elem& value) { DISPATCH(push_back(value)); }
-  void push_back(Elem&& value) { DISPATCH(push_back(std::move(value))); }
+  Elem& front() { return DISPATCH(front()); }
+  Elem& back() { return DISPATCH(back()); }
+  Elem& operator[](size_t index) { return DISPATCH(operator[](index)); }
+  const Elem& operator[](size_t index) const {
+    return DISPATCH(operator[](index));
+  }
+  Elem& at(size_t index) { return DISPATCH(at(index)); }
+  const Elem& at(size_t index) const { return DISPATCH(at(index)); }
+  void clear() { return DISPATCH(clear()); }
+  void push_back(const Elem& value) { return DISPATCH(push_back(value)); }
+  void push_back(Elem&& value) { return DISPATCH(push_back(std::move(value))); }
   template <typename... Args>
   void emplace_back(Args... args) {
-    DISPATCH(emplace_back(std::forward<Args>(args)...));
+    return DISPATCH(emplace_back(std::forward<Args>(args)...));
   }
-  void pop_front() { DISPATCH(pop_front()); }
-  size_t size() { DISPATCH(size()); }
-  size_t max_size() { DISPATCH(max_size()); }
-  bool empty() { DISPATCH(empty()); }
+  void pop_front() { return DISPATCH(pop_front()); }
+  size_t size() { return DISPATCH(size()); }
+  size_t max_size() { return DISPATCH(max_size()); }
+  bool empty() { return DISPATCH(empty()); }
 
   friend bool operator<(const RingBufAdapter& lhs, const RingBufAdapter& rhs) {
-    switch (lhs.variant_) {
-      case (Variant::Standard):
-        return lhs.static_ < rhs.static_;
-      case (Variant::Deque):
-        return lhs.dynamic_ < rhs.dynamic_;
-    }
+    return lhs.variant_ == Variant::Standard ? lhs.static_ < rhs.static_
+                                             : lhs.dynamic_ < rhs.dynamic_;
   }
 
   friend bool operator==(const RingBufAdapter& lhs, const RingBufAdapter& rhs) {
-    switch (lhs.variant_) {
-      case (Variant::Standard):
-        return lhs.static_ == rhs.static_;
-      case (Variant::Deque):
-        return lhs.dynamic_ == rhs.dynamic_;
-    }
+    return lhs.variant_ == Variant::Standard ? lhs.static_ == rhs.static_
+                                             : lhs.dynamic_ == rhs.dynamic_;
   }
 
   friend bool operator!=(const RingBufAdapter& lhs, const RingBufAdapter& rhs) {
-    switch (lhs.variant_) {
-      case (Variant::Standard):
-        return lhs.static_ != rhs.static_;
-      case (Variant::Deque):
-        return lhs.dynamic_ != rhs.dynamic_;
-    }
+    return lhs.variant_ == Variant::Standard ? lhs.static_ != rhs.static_
+                                             : lhs.dynamic_ != rhs.dynamic_;
   }
 
   void swap(RingBufAdapter& other) {
-    switch (variant_) {
-      case (Variant::Standard):
-        return static_.swap(other.static_);
-      case (Variant::Deque):
-        return dynamic_.swap(other.dynamic_);
-    }
+    return variant_ == Variant::Standard ? static_.swap(other.static_)
+                                         : dynamic_.swap(other.dynamic_);
   }
 
 #undef DISPATCH
