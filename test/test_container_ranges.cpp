f2e225cd63cb7920742c9e9deaa6f888ca2f1bd0
Dominic van Berkel <dominic@baudvine.net>
2022-01-04 16:20:16 +0100

    Upgrade the ringbuf iterator to bidirectional

--- test/test_container_ranges.cpp
+++ test/test_container_ranges.cpp
@@ -14,14 +14,14 @@ class ContainerRanges : public testing::Test {};
 
 TYPED_TEST_SUITE_P(ContainerRanges);
 
-TYPED_TEST_P(ContainerRanges, IsRandomAccessRange) {
-  EXPECT_TRUE(std::ranges::random_access_range<TypeParam>);
+TYPED_TEST_P(ContainerRanges, IsBidiRange) {
+  EXPECT_TRUE(std::ranges::bidirectional_range<TypeParam>);
 }
 
-REGISTER_TYPED_TEST_SUITE_P(ContainerRanges, IsRandomAccessRange);
+REGISTER_TYPED_TEST_SUITE_P(ContainerRanges, IsBidiRange);
 
 using Containers =
     ::testing::Types<baudvine::RingBuf<int, 3>, baudvine::DequeRingBuf<int, 3>>;
 INSTANTIATE_TYPED_TEST_SUITE_P(My, ContainerRanges, Containers);
 
-#endif // BAUDVINE_HAVE_RANGES
+#endif  // BAUDVINE_HAVE_RANGES
