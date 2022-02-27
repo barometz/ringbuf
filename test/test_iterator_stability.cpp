d4a1170bab5538a849eef0e2e522a522d4ffe413
Dominic van Berkel <dominic@baudvine.net>
2022-02-27 15:56:15 +0100

    Test actually existing iterator stability

--- test/test_iterator_stability.cpp
+++ test/test_iterator_stability.cpp
@@ -3,20 +3,9 @@
 
 #include "ringbufs.h"
 
-#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
 #include <iterator>
-
-namespace {
-template <typename T>
-T GetBuf() {
-  T result;
-  result.push_back(10);
-  result.push_back(20);
-  return result;
-}
-}  // namespace
 
 template <class RingBuf>
 class IteratorStability : public testing::Test {};
@@ -25,39 +14,48 @@ using RingBufs = AllRingBufs<int, 5>;
 // NOLINTNEXTLINE - clang-tidy complains about missing variadic args
 TYPED_TEST_SUITE(IteratorStability, RingBufs);
 
-TYPED_TEST(IteratorStability, PushBack) {
-  // When an element is pushed back, begin() should only change when we were full.
+TYPED_TEST(IteratorStability, PushFront) {
+  // push_front() does not move end()
   TypeParam underTest;
-  const auto begin = underTest.begin();
-  
-  std::fill_n(std::back_inserter(underTest), 4, 0);
-  EXPECT_EQ(begin, underTest.begin());
-  
-  underTest.push_back(0);
-  EXPECT_EQ(begin, underTest.begin());
 
-  underTest.push_back(0);
-  EXPECT_NE(begin, underTest.begin());
+  const auto end = underTest.end();
+  for (size_t i = 0; i < underTest.max_size(); i++) {
+    underTest.push_front(5);
+    EXPECT_EQ(end, underTest.end());
+  }
+}
+
+TYPED_TEST(IteratorStability, PushBack) {
+  // push_back() does not move begin()
+  TypeParam underTest;
+
+  const auto begin = underTest.begin();
+  for (size_t i = 0; i < underTest.max_size(); i++) {
+    underTest.push_back(5);
+    EXPECT_EQ(begin, underTest.begin());
+  }
 }
 
 TYPED_TEST(IteratorStability, PopFront) {
-  // When an element is pushed back, begin() should only change when we were full.
+  // pop_front does not move end().
   TypeParam underTest;
-  
   std::fill_n(std::back_inserter(underTest), underTest.max_size() + 2, 0);
-  const auto end = underTest.end();
-  underTest.pop_front();
-  EXPECT_EQ(end, underTest.end());
-  EXPECT_FALSE(end < underTest.end());
-  EXPECT_FALSE(end > underTest.end());
-  EXPECT_EQ(0, end - underTest.end());
 
-  underTest.pop_front();
-  underTest.pop_front();
-  underTest.pop_front();
-  underTest.pop_front();
-  EXPECT_EQ(end, underTest.end());
-  EXPECT_FALSE(end < underTest.end());
-  EXPECT_FALSE(end > underTest.end());
-  EXPECT_EQ(0, end - underTest.end());
+  const auto end = underTest.end();
+  for (size_t i = 0; i < underTest.max_size(); i++) {
+    underTest.pop_front();
+    EXPECT_EQ(end, underTest.end());
+  }
+}
+
+TYPED_TEST(IteratorStability, PopBack) {
+  // pop_back does not move begin().
+  TypeParam underTest;
+  std::fill_n(std::back_inserter(underTest), underTest.max_size() + 2, 0);
+
+  const auto begin = underTest.begin();
+  for (size_t i = 0; i < underTest.max_size(); i++) {
+    underTest.pop_back();
+    EXPECT_EQ(begin, underTest.begin());
+  }
 }
